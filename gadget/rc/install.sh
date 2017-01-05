fc9c95863b3fd42989d9f2b36e57d29c0ee48b5f
Darryl Burke <darrylb@burke-consulting.net>
2017-01-05 12:22:35 -0500

    Update install.sh

--- gadget/rc/install.sh
+++ gadget/rc/install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cp pyreboot /etc/init.d
 cp pychange /etc/init.d
+chmod 755 /etc/init.d/pyreboot
+chmod 755 /etc/init.d/pychange
 update-rc.d pyreboot defaults
 update-rc.d pychange defaults
