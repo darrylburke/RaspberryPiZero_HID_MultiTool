f83258d210514c7919688deeb6418f92a87fde22
Darryl Burke <darrylb@burke-consulting.net>
2017-01-05 12:21:27 -0500

    debugging change script

--- gadget/change.sh
+++ gadget/change.sh
@@ -2,9 +2,10 @@
 CURRENT=`cat current`
 NEW=`python checkfunction.py`
 OS=`python checkos.py`
+DATE=`date`
 if [ "$CURRENT" != "$NEW" ];then
 # echo ./$NEW.sh $OS
-echo "OLD:[ $CURRENT ] NEW:[ $NEW ] OS:[ $OS] "
+echo "$DATE OLD:[ $CURRENT ] NEW:[ $NEW ] OS:[ $OS] " | tee -a /tmp/change
  ./$NEW.sh $OS
  reboot
 fi
