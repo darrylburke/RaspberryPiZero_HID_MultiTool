e819962b75828f50aca6212162fa2176c7231aa4
Darryl Burke <darrylb@burke-consulting.net>
2017-01-05 12:26:06 -0500

    Update ethernet.sh

--- gadget/ethernet.sh
+++ gadget/ethernet.sh
@@ -2,7 +2,7 @@
 OS=$1
 CURRENT=`cat current`
 (cd $CURRENT;./stop.sh)
-cp serial/config.txt /boot
-cp serial/cmdline.txt /boot
-(cd serial;./start.sh)
+cp ethernet/config.txt /boot
+cp ethernet/cmdline.txt /boot
+(cd ethernet;./start.sh)
 
